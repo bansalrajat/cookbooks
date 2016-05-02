1e585c3735eb676c0b013e631622a057c95d4eeb
rajat <b.rajat@yahoo.co.in>
2016-05-02 18:55:17 +0530

    cb created

---
+++ scm_java/CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.1.0
+
+Initial release of scm_java
---
+++ scm_java/metadata.rb
@@ -0,0 +1,7 @@
+name             'scm_java'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures scm_java'
+long_description 'Installs/Configures scm_java'
+version          '0.1.0'
