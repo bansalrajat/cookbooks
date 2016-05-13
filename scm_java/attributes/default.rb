895f3633401ab3918106355877eac76ef947cb91
rajat <b.rajat@yahoo.co.in>
2016-05-13 17:27:48 +0530

    deleting java

--- scm_java/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default['scm_java']['installation_path'] = "/usr/java"
-default['scm_java']['artifactory_url'] = "artifactory.demo.hcentive.com/artifactory/chef-repo/java"
-default['scm_java']['java_tar_name'] = "jdk-8u45-linux-x64.tar.gz"
